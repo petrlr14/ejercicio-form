1bde52fc8748e6a378fde4d99d7cf53fa6908a8c
petrlr14 <00057616@uca.edu.sv>
2020-02-20 13:54:45 -0600

    partial

---
+++ src/components/List/Item/Item.js
@@ -0,0 +1,14 @@
+/* <div className={container}>
+      <div className={sub}>
+        <h4></h4>
+        <span
+          className={deleteBtn}
+        >
+          x
+        </span>
+      </div>
+      <div className={sub}>
+        <span></span>
+        <span></span>
+      </div>
+    </div> */
